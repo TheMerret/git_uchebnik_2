e4ffaf168ca09a0f5df82c2143fa74f8d908b5c2
TheMerret <merret@bk.ru>
2021-12-06 18:20:52 +0300

    added main

---
+++ main.py
@@ -0,0 +1,14 @@
+def my_superfunction():
+    print("What an awesome print!")
+
+
+def main():
+    print("My first git pogramm")
+    print("And i chage it every day")
+    print('Again')
+    print('UFO came and added this line')
+    my_superfunction()
+
+
+if __name__ == '__main__':
+    main()
